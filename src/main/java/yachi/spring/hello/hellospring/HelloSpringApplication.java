ee7e5d0142d6c33ad74f2e34db453b7df9831307
Yachi Wu <wuyachi@163.com>
2019-02-22 22:27:03 +0800

    set port

--- src/main/java/yachi/spring/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/yachi/spring/hello/hellospring/HelloSpringApplication.java
@@ -2,12 +2,14 @@ package yachi.spring.hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloSpringApplication {
+public class HelloSpringApplication implements WebServerFactoryCustomizer<ConfigurableWebServerFactory>{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
@@ -18,4 +20,7 @@ public class HelloSpringApplication {
 		return "hello Spring!";
 	}
 
+	public void customize(ConfigurableWebServerFactory factory) {
+		factory.setPort(8081);
+	}
 }
